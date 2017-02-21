3e69d028eaa6b0a3a0148410a945c5a0fadcf139
Ian Carroll <icarroll@sesync.org>
2017-02-20 22:10:00 -0500

    make version relevant in dependencies

--- dependencies.R
+++ dependencies.R
@@ -1,8 +1,23 @@
-requirements <- c('datasets')
+# List the package and version as below
+dependencies <- read.csv(textConnection("
+  Package,     Min.Version
+  dplyr,       0.5.0
+  raster,      2.5.8
+  ggplot2,     2.0.1
+  "), stringsAsFactors=FALSE, strip.white=TRUE)
 
-missing <- setdiff(requirements, rownames(installed.packages()))
-if (length(missing) != 0) {
-  install.packages(missing, repos="http://cran.us.r-project.org")
-}
+## No changes necessary below. ##
 
-lapply(requirements, require, character.only=TRUE)
+# Compare requirements to installed packages
+pkgs <- merge(pkgs,
+              data.frame(installed.packages(), stringsAsFactors=FALSE),
+              by="Package", all.x=TRUE)
+
+# Filter out packages meeting minimum version requirement
+pkgs <- pkgs[mapply(compareVersion, pkgs$Min.Version, pkgs$Version) > 0, ]
+
+# Install missing and newer packages
+lapply(pkgs$Package, install.packages, repos="http://cran.us.r-project.org")
+
+# Require dependencies
+lapply(dependencies$Package, require, character.only=TRUE)
