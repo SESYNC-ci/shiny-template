e5ec02b1a2d67cb61e43f3a3661ad02bb638883d
Ian Carroll <icarroll@sesync.org>
2017-08-18 11:47:32 -0400

    eliminate rownames warning

--- dependencies.R
+++ dependencies.R
@@ -4,14 +4,17 @@ dependencies <- read.csv(textConnection("
   dplyr,       0.5.0
   raster,      2.5.8
   ggplot2,     2.0.1
-  "), stringsAsFactors=FALSE, strip.white=TRUE)
+  "), stringsAsFactors = FALSE, strip.white = TRUE)
 
 ## No changes necessary below. ##
 
+# Import installed package versions
+pkgs <- installed.packages()
+rownames(pkgs) <- c()
+pkgs <- data.frame(pkgs, stringsAsFactors = FALSE)
+
 # Compare requirements to installed packages
-pkgs <- merge(pkgs,
-              data.frame(installed.packages(), stringsAsFactors=FALSE),
-              by="Package", all.x=TRUE)
+pkgs <- merge(dependencies, pkgs, by="Package", all.x=TRUE)
 
 # Filter out packages meeting minimum version requirement
 pkgs <- pkgs[mapply(compareVersion, pkgs$Min.Version, pkgs$Version) > 0, ]
@@ -19,5 +22,5 @@ pkgs <- pkgs[mapply(compareVersion, pkgs$Min.Version, pkgs$Version) > 0, ]
 # Install missing and newer packages
 lapply(pkgs$Package, install.packages, repos="http://cran.us.r-project.org")
 
-# Require dependencies
+# Require dependencies [optional]
 lapply(dependencies$Package, require, character.only=TRUE)
